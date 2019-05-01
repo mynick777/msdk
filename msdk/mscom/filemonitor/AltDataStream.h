230c734b52d5147b0416e41b83b0a19d7db974f5
mynick777 <mynick777@gmail.com>
2019-05-02 00:00:37 +0800

    2

--- msdk/mscom/filemonitor/AltDataStream.h
+++ msdk/mscom/filemonitor/AltDataStream.h
@@ -31,3 +31,58 @@ private:
 };
 
 
+//int main()
+//{
+//	CAltDataStream ads;
+//	ads.SetFile(L"D:\\ChromeDownload\\¤U¸ü (6).jpg");
+//	CPtrList streams;
+//	ads.GetStreams(streams);
+//
+//	TCHAR szData[MAX_STREAM_DATA_PREVIEW_SIZE + 2];
+//	DWORD dwSize;
+//
+//	for (int nIndex = 0; nIndex < streams.GetCount(); nIndex++)
+//	{
+//		POSITION pos = streams.FindIndex(nIndex);
+//
+//		LVITEM lvItem;
+//		STREAM_INFO* pStream = (STREAM_INFO*)streams.GetAt(pos);
+//		TCHAR		 szBuffer[20];
+//
+//		// Stream Name
+//		lvItem.mask = LVIF_TEXT;
+//		lvItem.iItem = nIndex;
+//		lvItem.iSubItem = 0;
+//		lvItem.pszText = pStream->sName.GetBuffer(0);
+//
+//		// Size
+//		_itot_s(pStream->lSize, szBuffer, 10);
+//
+//		// Data
+//		if (pStream->lSize > MAX_STREAM_DATA_PREVIEW_SIZE)
+//		{
+//			dwSize = MAX_STREAM_DATA_PREVIEW_SIZE;
+//		}
+//		else
+//		{
+//			dwSize = pStream->lSize;
+//		}
+//
+//		ZeroMemory(szData, (MAX_STREAM_DATA_PREVIEW_SIZE + 2) * sizeof(TCHAR));
+//
+//		for (int nIndex = 0; nIndex < pStream->lSize; nIndex++)
+//		{
+//			szData[nIndex] = pStream->baData[nIndex];
+//		}
+//	}
+//
+//	std::wstring check(szData);
+//
+//	if (std::string::npos != check.find_first_of(L"ZoneId=3"))
+//	{
+//		int get = 1;
+//	}
+//	ads.ReleaseStreams(streams);
+//	return 0;
+//}
+
